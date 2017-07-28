6e0348db177ed87470ba4ad31451bd58697cf79a
Keith <keithtmccartney@hotmail.com>
2017-07-28 20:54:45 +0100

    Adding Selenium.Support, Selenium.WebDriver packages.

---
+++ ConsumerApi/ConsumerApi.Tests/SupportFiles/PropertiesCollection.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerApi.Tests.SupportFiles
+{
+    class PropertiesCollection
+    {
+        public static IWebDriver driver
+        {
+            get; set;
+        }
+    }
+}
