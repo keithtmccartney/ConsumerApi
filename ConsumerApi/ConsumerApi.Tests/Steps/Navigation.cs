bdb56edec8c1a7fe5d05cfcc8aa8a4705b00c6e6
Keith <keithtmccartney@hotmail.com>
2017-07-28 21:36:24 +0100

    Adding foundations of 'Navigation' Steps test.

---
+++ ConsumerApi/ConsumerApi.Tests/Steps/Navigation.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace ConsumerApi.Tests.Steps
+{
+    [Binding]
+    public class Navigation
+    {
+        [Given(@"I have...")]
+        public void Abc()
+        {
+        }
+
+        [When(@"I see...")]
+        public void Def()
+        {
+        }
+
+        [Then(@"I should...")]
+        public void Ghi()
+        {
+        }
+    }
+}
