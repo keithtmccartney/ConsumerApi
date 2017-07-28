f8b33236cc447b739a3ef8ef7df2f0ead8bdd1c9
Keith <keithtmccartney@hotmail.com>
2017-07-28 20:28:37 +0100

    Committing initial project with stock unit testing.

---
+++ ConsumerApi/ConsumerApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConsumerApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ConsumerApi/ConsumerApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ConsumerApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
